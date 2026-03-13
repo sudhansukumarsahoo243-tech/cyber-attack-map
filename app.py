095a8764706f6cfb959cc6451666096fe54b4124
sudhansukumarsahoo243-tech <sudhansukumarsahoo243@gmail.com>
2026-03-13 20:02:30 +0530

    fix attacks api

--- app.py
+++ app.py
@@ -1,27 +1,6 @@
-from flask import Flask, render_template, jsonify
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-API_KEY = "ff3256c3294307966e182d60b39f4398db746b3cc0da659897a5066f3a3b0428d2ca6fdacc48b493"
-
-# Get malicious IP list
-def get_attacks():
-
-    url = "https://api.abuseipdb.com/api/v2/blacklist"
-
-    headers = {
-        "Key": API_KEY,
-        "Accept": "application/json"
-    }
-
-    params = {
-        "confidenceMinimum": 90
-    }
-
-    response = requests.get(url, headers=headers, params=params)
-
-    return response.json()
 
 @app.route("/")
 def home():
@@ -29,10 +8,14 @@ def home():
 
 @app.route("/attacks")
 def attacks():
-
-    data = get_attacks()
-
-    return jsonify(data)
+    return {
+        "data": [
+            {"ipAddress": "8.8.8.8"},
+            {"ipAddress": "1.1.1.1"},
+            {"ipAddress": "185.143.223.12"},
+            {"ipAddress": "45.33.32.156"}
+        ]
+    }
 
 if __name__ == "__main__":
     app.run()
