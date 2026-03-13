a5bc92a4ddf12ead34b97025c69eb44e05347486
sudhansukumarsahoo243-tech <sudhansukumarsahoo243@gmail.com>
2026-03-13 20:13:15 +0530

    fix frontend api

--- static/script.js
+++ static/script.js
@@ -1,12 +1,23 @@
+var map = L.map('map').setView([20,0],2);
+
+L.tileLayer(
+'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+).addTo(map);
+
+
 fetch("/attacks")
 .then(res => res.json())
 .then(data => {
+
+    console.log(data)
 
     data.data.slice(0,20).forEach(ip => {
 
         fetch("https://ipinfo.io/" + ip.ipAddress + "/json")
         .then(res => res.json())
         .then(loc => {
+
+            if(!loc.loc) return;
 
             var coords = loc.loc.split(",")
 
